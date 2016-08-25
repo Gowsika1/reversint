5d49a9ae1d1024099175d9bef822248140bf2a11
Gowsika1 <gowsikakumaravel@gmail.com>
2016-08-25 09:18:30 +0530

    Create deleting3digit.java

---
+++ deleting3digit.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+
+public class deleting3digit
+{
+
+    public static void main(String[] args)
+    {
+    Scanner sc=new Scanner(System.in);
+    int n=sc.nextInt();
+    String s=String.valueOf(n);
+    char[] c=s.toCharArray();
+    for(int i=0;i<c.length;i++)
+    Arrays.sort(c);
+    for(int i=1;i<c.length;i++)
+    {
+        System.out.println(c[i+1]);
+    }
+    }
+}
